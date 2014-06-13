7688e68eeb9d9ca1597827cc52532f271191bdf5
Perry Yan <perry.c.yan@gmail.com>
2014-06-13 16:51:12 -0700

    fix merge bugs in reservation

--- reservation.php
+++ reservation.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!--Script for toggling flight details-->
 <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
 <script>
@@ -9,8 +8,6 @@
     	});
 	});
 </script>
-
-=======
 <!DOCTYPE html>
 
 <head>
@@ -66,54 +63,15 @@ if ($db_conn) {
  	if (array_key_exists('flightchoice', $_POST)) {
  		// test YVR to TPE (country TW) in flights.php it has 1 and 2 transfers
  		// direct flight I always test with YVR to HKG (country HK) but doesnt matter
-<<<<<<< HEAD
+
 		$route = unserialize($_POST['flightchoice']);
 		print_r($route);
 		printDetails($route, 0);
-=======
+
 		$flight = unserialize($_POST['flightchoice']);
 		print_r($flight);
->>>>>>> 67d46c7d238529e16d4d7c41e1961d267a6cfa0a
+
 	}
 }
-
-// Function to parse flight.
-// Returns an array with the flight tuple and number of transfers
-/*
-function parseFlight($flight) {
-		
-		$flight_tuple = explode(' ', $flight);
-		
-		// Indexes
-		$num_transfer = 0;
-		$num_airport = 0;
-		$num_
-
-		while (list($key, $value) = each($flight_tuple)) {
-			
-			// Counts the number of transfers
-			if(match_5_digits($value)) {
-				$parsed_flight['flight'.$num_transfer] = $value;
-				$num_transfer++;
-			}
-			
-			// Airports. Will always be in order, so AP0 -> AP1 -> AP2 etc.
-			if(match_airport_format($value)) {
-				$parsed_flight['ap'.$num_airport] = $value;
-				$num_airport++;
-			}
-			
-			if(match_yymmdd_format($value)) {
-				
-			}
-			
-		}
-		
-		
-		$parsed_flight['num_transfer'] = $num_transfer;
-		
-		return $flight_tuple;
-		
-}*/
  
 ?>
