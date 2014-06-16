ab02d0d2e63bdd9f07d8e8ba3586fade6b8c0707
unknown <User@.(none)>
2014-06-15 20:50:58 -0700

    Added input validity checks, bug fix

--- admin.php
+++ admin.php
@@ -7,7 +7,7 @@
 		<option selected value = "default">(Choose a data table)</option>
 		<option value = "CUSTOMER">Customers</option>
 		<option value = "AIRPORT">Airports</option>
-		<option value = "PLANE_IN">Airplanes</option>
+		<option value = "PLANE">Airplanes</option>
 		<option value = "FLIGHT">Flights</option>
 		<option value = "MAKE_RES">Reservations</option>
 		<option value = "RES_INCLUDES">Reserv-Flights*</option>
@@ -19,77 +19,61 @@
 	<noscript><input type="submit" value="Submit"></noscript></p>
 </form>
 
+<script>
+	function validateInsert() {
+		var elements = document.forms["insertform"].elements;
+	    for (e in elements) {
+	    	if (elements[e].name == "tablename") break;
+	    	var validity = isValid(elements[e].name, elements[e].value);
+	    	if (validity !== true) {
+	    		alert("Invalid input to regex: " + validity);
+	    		return false;
+	    	}
+	    }
+   		 return true;	
+	}
+	
+	function isValid(name, value) {
+		var pattern;
+		switch (name) {
+		case "RESID":
+		case "RESORDER":
+		case "BID":
+		case "STATUS":
+		case "CID":
+		case "PCLASS":
+		case "TICKET_NUM":
+		case "FID":
+		case "PAYID":
+		case "CREDITCARD":
+	    case "PHONE":
+	    case "COST":
+	    case "TOTAL_COST":
+		case "PASSPORT_NUM":
+		case "CID": pattern = /(^\d\d*\d$)|(^\d$)/; break;
+		case "WEIGHT_KG": pattern = /(^(\d*)\.{0,1}(\d*)$)|(^\d$)/; break;
+		case "EMAIL": pattern = /^\w\w*(\.\w+)*@\w+\.\w+$/; break;
+		case "DEPARTAP":
+		case "ARRIVALAP":
+		case "CODE": pattern = /^[a-zA-Z][a-zA-Z][a-zA-Z]$/; break;
+		case "COUNTRY":
+		case "PASSPORT_COUNTRY": pattern = /^[a-zA-Z][a-zA-Z]$/; break;
+		case "DEPARTTIME":
+		case "ARRIVALTIME": pattern = /.+/; break;			
+		default: pattern = /^\w\w*\w$/;
+		}
+		if (!pattern.test(value)) return name + " " + pattern;
+		else return true; 
+	}
+</script>
 <?php
 
 // These stuff are needed (for now) to connect Oracle, will figure out how to import from
 // main php file
 $success = True; //keep track of errors so it redirects the page only if there are no errors
-$db_conn = OCILogon("ora_b4s8", "a16894123", "ug");
+//$db_conn = OCILogon("ora_b4s8", "a16894123", "ug");
 
-// ExecutePlainSQL, executeBoundSQL are from a sample file in the CS304 Tutorial page, 
-// http://www.ugrad.cs.ubc.ca/~cs304/2014S1/index.html,
-// Author: Jiemin Zhang,
-// Modified by Simona Radu, Tam Ho
-function executePlainSQL($cmdstr) { //takes a plain (no bound variables) SQL command and executes it
-	//echo "<br>running ".$cmdstr."<br>";
-	global $db_conn, $success;
-	$statement = OCIParse($db_conn, $cmdstr); //There is a set of comments at the end of the file that describe some of the OCI specific functions and how they work
-
-	if (!$statement) {
-		echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
-		$e = OCI_Error($db_conn); // For OCIParse errors pass the       
-		// connection handle
-		echo htmlentities($e['message']);
-		$success = False;
-	}
-
-	$r = OCIExecute($statement, OCI_DEFAULT);
-	if (!$r) {
-		echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
-		$e = oci_error($statement); // For OCIExecute errors pass the statementhandle
-		echo htmlentities($e['message']);
-		$success = False;
-	} else {
-	}
-	return $statement;
-
-}
-
-function executeBoundSQL($cmdstr, $list) {
-	/* Sometimes a same statement will be excuted for severl times, only
-	 the value of variables need to be changed.
-	 In this case you don't need to create the statement several times; 
-	 using bind variables can make the statement be shared and just 
-	 parsed once. This is also very useful in protecting against SQL injection. See example code below for       how this functions is used */
-
-	global $db_conn, $success;
-	$statement = OCIParse($db_conn, $cmdstr);
-
-	if (!$statement) {
-		echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
-		$e = OCI_Error($db_conn);
-		echo htmlentities($e['message']);
-		$success = False;
-	}
-
-	foreach ($list as $tuple) {
-		foreach ($tuple as $bind => $val) {
-			//echo $val;
-			//echo "<br>".$bind."<br>";
-			OCIBindByName($statement, $bind, $val);
-			unset ($val); //make sure you do not remove this. Otherwise $val will remain in an array object wrapper which will not be recognized by Oracle as a proper datatype
-
-		}
-		$r = OCIExecute($statement, OCI_DEFAULT);
-		if (!$r) {
-			echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
-			$e = OCI_Error($statement); // For OCIExecute errors pass the statementhandle
-			echo htmlentities($e['message']);
-			echo "<br>";
-			$success = False;
-		}
-	}
-}
+include "oci_functions.php"; 
 
 // Same source as above, modified for generic table printing
 // Prints everything after table is selected
@@ -130,12 +114,14 @@ function printTable($attributes, $data) {
 
 // Prints the form to insert new tuple into chosen table
 function printInsertFields($table, $attributes) {
-	$form = "<form method='POST' action='admin.php'><table>";
+	$form = "<form method='POST' name='insertform' action='admin.php' onsubmit='return validateInsert()'><table>";
 	for ($it=0; $it < count($attributes); $it++) {
-		$form = $form . "<tr><td>" . $attributes[$it] . ":</td><td><input type='text' name = '$it'></td></tr>";		
+		if (strcmp($attributes[$it],"DEPARTTIME") == 0 || strcmp($attributes[$it],"ARRIVALTIME") == 0) $type = "datetime-local";
+		else $type = "text";
+		$form = $form . "<tr><td>" . $attributes[$it] . ":</td><td><input type='$type' name = '$attributes[$it]' required></td></tr>";		
 	} 
 	echo $form . "</table>
-				  <p><input type='hidden' value='$table' name='$it'></p>
+				  <p><input type='hidden' value='$table' name='tablename'></p>
 				  <p><input type='submit' value='Insert' name='insertsubmit'></p>
 			      </form>";
 }
@@ -188,14 +174,18 @@ if ($db_conn) {
 	}
 	// Handle tuple insert form submission
 	if (array_key_exists('insertsubmit', $_POST)) {
-		$tuple = "";
-		if(count($_POST) > 1) $tuple = "'$_POST[0]'";
-		$it = 1;	
-		while ($it < count($_POST)-2) {
-			$tuple = "$tuple,'$_POST[$it]'";
-			$it = $it + 1;
+		reset($_POST);		
+		list($key, $value) = each($_POST);
+		$tuple = "'$value'";
+		while (list($key, $value) = each($_POST)) {
+			if (strcmp($key,"DEPARTTIME") == 0 || strcmp($key,"ARRIVALTIME") == 0) {
+				$value = parseDate($value,3);
+			}
+			if (strcmp($key,"insertsubmit") == 0 || strcmp($key,"tablename") == 0) break;
+			$tuple = "$tuple,'$value'";
 		}
-		executePlainSQL("insert into $_POST[$it] values ($tuple)");
+		$tablename = $_POST['tablename'];
+		executePlainSQL("insert into $tablename values ($tuple)");
 		OCICommit($db_conn);
 	}
 	// Handle tuple update form submission
@@ -251,6 +241,5 @@ if ($db_conn) {
 		}
 	}
 	OCILogoff($db_conn);
-	
 }
 ?>
