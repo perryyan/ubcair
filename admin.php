8ed7db9a7d56b1736c5941ee2d1562969d32236b
Perry <perry.c.yan@gmail.com>
2014-06-18 18:50:28 -0700

    Add styles for admin

--- admin.php
+++ admin.php
@@ -1,4 +1,41 @@
-<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css">
+<!DOCTYPE html>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="UBC Airline Booking Service">
+	<title>UBC Air</title>
+	<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css">
+	<link rel="stylesheet" href="css/mainpage.css"
+</head>
+
+<body>
+<div class="header">
+    <div class="home-menu pure-menu pure-menu-open pure-menu-horizontal pure-menu-fixed">
+        <a class="pure-menu-heading" href="">UBC Air</a>
+        <ul>
+            <li><a href='index.php'>Home</a></li>
+<?php
+
+	if(!array_key_exists('loggedin', $_COOKIE) ) {
+		echo "<li><a href='login.php'>Login</a></li>";
+		echo "<li><a href='register.php'>Sign Up</a></li>";
+		header('location: login.php');
+	}
+	else {
+		echo "<li><a href='logout.php'>Logout</a></li>";
+		echo "<li><a href='support.php'>My Orders</a></li>";		
+	}   
+?>    
+            <li><a href='flights.php'>Find flights</a></li>
+        </ul>
+    </div>
+</div>
+
+
+
+<body>
+<div class="content-customer-area">
 
 <!-- Drop down menu for selecting specific table -->
 <p>Select working data table</p>
